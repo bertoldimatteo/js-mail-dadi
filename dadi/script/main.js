3bf6bd99c6b7033064e6f2c493f6f274b06915b6
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-03 16:25:57 +0200

    aggiunto bottone per giocare

--- dadi/script/main.js
+++ dadi/script/main.js
@@ -1,16 +1,24 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-const userNumber = Math.floor((Math.random() * 6) + 1);
-const pcNumber = Math.floor((Math.random() * 6) + 1);
 
-const userResult = document.getElementById("userResult").innerHTML = userNumber;
-const pcResult = document.getElementById("pcResult").innerHTML = pcNumber;
+const play = document.querySelector(".play");
 
-if ( userNumber > pcNumber) {
-    console.log("Hai vinto");
-    const result = document.getElementById("result").innerHTML = "Hai Vinto!"
-} else {
-    console.log("Hai perso");
-    const result = document.getElementById("result").innerHTML = "Hai Perso!"
-}
+play.addEventListener("click", 
+    function (){
+        const userNumber = Math.floor((Math.random() * 6) + 1);
+        const pcNumber = Math.floor((Math.random() * 6) + 1);
+
+        const userResult = document.getElementById("userResult").innerHTML = userNumber;
+        const pcResult = document.getElementById("pcResult").innerHTML = pcNumber;
+
+        if ( userNumber > pcNumber) {
+            console.log("Hai vinto");
+            const result = document.getElementById("result").innerHTML = "Hai Vinto!"
+        } else {
+            console.log("Hai perso");
+            const result = document.getElementById("result").innerHTML = "Hai Perso!"
+        }
+    }
+);
+
