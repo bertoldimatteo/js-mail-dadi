f976d83a7f16f3e458928df30c82e54750edae3b
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-03 14:58:18 +0200

    creato istruzione condizionale per verificare mail su array

--- mail/script/main.js
+++ mail/script/main.js
@@ -5,3 +5,11 @@
 
 // creo array
 const mail = ["luca.lanarti@gmail.com", "antonio.rosita@hotmail.it", "pasquale.torrente@libero.it"];
+// creo finestra per user input
+const userInput = prompt("Inserisci la tua email");
+
+if (mail.includes(userInput)) {
+    console.log("Welcome back, type password for log-in.");
+} else {
+    console.log("User is not registered.");
+};
