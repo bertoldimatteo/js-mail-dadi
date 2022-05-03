83f72caace956aaad14677233035fd76c5884488
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-03 14:47:29 +0200

    creata array con 3 mail

---
+++ mail/script/main.js
@@ -0,0 +1,7 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// creo array
+const mail = ["luca.lanarti@gmail.com", "antonio.rosita@hotmail.it", "pasquale.torrente@libero.it"];
