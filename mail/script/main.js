93295612373fa130cd3b8b3bdc9cf18d79b3eeb2
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-03 15:44:53 +0200

    aggiunto fomr con lettura

--- mail/script/main.js
+++ mail/script/main.js
@@ -5,11 +5,19 @@
 
 // creo array
 const mail = ["luca.lanarti@gmail.com", "antonio.rosita@hotmail.it", "pasquale.torrente@libero.it"];
-// creo finestra per user input
-const userInput = prompt("Inserisci la tua email");
 
-if (mail.includes(userInput)) {
-    console.log("Welcome back, type password for log-in.");
-} else {
-    console.log("User is not registered.");
-};
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", 
+    function() {
+        const userInput = document.getElementById("userInput").value;
+        const userPwd = document.getElementById("userPwd");
+        if (mail.includes(userInput)) {
+            userPwd.classList.add("showClass")
+        } else {
+            console.log("User is not registered.");
+        };
+    }
+);
+
+
